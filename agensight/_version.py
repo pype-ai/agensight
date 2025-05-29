0339b3ad42d5c4e7e8569c5bb02ec00a5b5854e9
DARKNESS <144640975+deepeshAg@users.noreply.github.com>
2025-05-29 18:09:58 +0530

    installation fixes

--- agensight/_version.py
+++ agensight/_version.py
@@ -1,7 +1,7 @@
 
 """Version information for agensight package."""
 
-__version__ = "0.5.4"
+__version__ = "0.1.1"
 __version_info__ = tuple(int(i) for i in __version__.split('.') if i.isdigit())
 
 # Build information (will be updated by CI/CD)
